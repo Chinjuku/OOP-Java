2d7ea8add51c91e0f39d46469f65189538603f06
Chinjuku <112687616+Chinjuku@users.noreply.github.com>
2023-05-05 01:23:44 +0700

    Update CheckingAccount.java

--- Lab 9/CheckingAccount.java
+++ Lab 9/CheckingAccount.java
@@ -1,44 +1,61 @@
 public class CheckingAccount extends Account{
-    private double credit;
-    public CheckingAccount () {
+      private double credit;
+      
+      public CheckingAccount(){
           super(0, "");
           this.credit = 0;
-    }
-    public CheckingAccount (double balance, String name, double credit) {
+      }
+      public CheckingAccount(double balance, String name, double credit){
           super(balance, name);
-          this.credit = credit;
-    }
-    public void setCredit (double credit) {
-          if (credit > 0) {
-                this.credit = credit;
+          this.setCredit(credit);
+      }
+      public void setCredit(double credit){
+          if (credit > 0){
+              this.credit = credit;
+          }else{
+              System.out.println("Input number must be a positive integer.");
           }
-          else {
-                System.out.println("Input number must be a positive integer.");
-          }
-    }
-    public double getCredit () {
+      }
+      public double getCredit(){
           return this.credit;
-    }
-    public void withdraw (double amount) throws WithdrawException{
-          if (this.getBalance() - amount + this.getCredit() < 0) {
-                // System.out.println("Not enough money!");
-                throw new WithdrawException("Account " + this.name +" has not enough money.");
+      }
+      
+      @Override
+      public void withdraw(double a)throws WithdrawException{
+          if (a<=0){
+              System.out.println("Input number must be a positive integer.");
+              
+          }else if(this.getBalance() >= a){
+              this.setBalance(this.balance - a);
+              System.out.println(a + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
+          }else if(this.getBalance() + this.getCredit() >= a){
+              this.setCredit(this.getCredit()- (a - this.getBalance()));
+              this.setBalance(0);
+              System.out.println(a + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
+          }else{
+              throw new WithdrawException("Account "+this.getName()+" has not enough money.");
           }
-          else {
-                if (this.getBalance() - amount >= 0) {
-                      this.setBalance(this.getBalance() - amount);
-                }
-                else {
-                      this.setCredit(this.getCredit() - amount);
-                      this.setBalance(0);
-                }
-                System.out.println(amount + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
-          }
-    }
-    public String toString () {
+      }
+      public void withdraw(String a) throws WithdrawException{
+          double b = Double.parseDouble(a);
+          withdraw(b);
+  //        if (b<=0){
+  //            System.out.println("Input number must be a positive integer.");
+  //            
+  //        }else if(this.getBalance() >= b){
+  //            this.setBalance(this.balance - b);
+  //            System.out.println(b + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
+  //        }else if(this.getBalance() + this.getCredit() >= b){
+  //            
+  //            this.setCredit(this.getCredit()- (b - this.getBalance()));
+  //            this.setBalance(0);
+  //            System.out.println(b + " baht is withdrawn from " + this.getName() + " and your credit balance is " + this.getCredit() + ".");
+  //        }else{
+  //            throw new WithdrawException("Account "+this.getName()+" has not enough money.");
+  //        }
+      }
+      @Override
+      public String toString(){
           return "The " + this.getName() + " account has " + this.getBalance() + " baht and " + this.getCredit() + " credits.";
-    }
-    public void withdraw(String amount) throws WithdrawException{
-        withdraw(Double.parseDouble(amount));
-    }
-}
+      }
+  }
